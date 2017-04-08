947bcbc1342b8b1e0a1655926798076b494004c6
carnellj <john.carnell@inin.com>
2017-04-08 10:21:41 -0400

    Latest edits

--- confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -1,10 +1,10 @@
 package com.thoughtmechanix.confsvr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
